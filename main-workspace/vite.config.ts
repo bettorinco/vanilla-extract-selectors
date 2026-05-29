573698de638231c8263f625045b224998638ae9c
bettorinco <albertogrisafi@live.com>
2026-05-29 07:29:27 +0200

    project setup

---
+++ main-workspace/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import {vanillaExtractPlugin} from "@vanilla-extract/vite-plugin";
+import react from "@vitejs/plugin-react";
+
+
+export default defineConfig({
+    plugins: [
+        vanillaExtractPlugin(),
+        react()
+    ]
+})
