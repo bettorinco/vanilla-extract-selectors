596b70b0069fe92b36ed54f13ca98feae9f993f5
Alberto <110282911+bettorinco@users.noreply.github.com>
2026-05-29 13:11:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { parentRecipe, childClass } from "./styles.css";
 export default function App() {
   return (
     <>
-      <label className={parentRecipe()}>
+      <div className={parentRecipe()}>
         <div className={childClass}>Child regular</div>
-      </label>
-      <label className={parentRecipe({ type: "special" })}>
+      </div>
+      <div className={parentRecipe({ type: "special" })}>
         <div className={childClass}>Child special</div>
-      </label>
+      </div>
     </>
   );
 }
